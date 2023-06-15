e6c6e1aa7aefd9bb9fded21eb24b683b7d45ddb4
“lucianobonilla27” <lucianobonilla27@gmail.com>
2023-06-15 19:08:33 -0300

    tercer commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using funciones;
 
-personaje pj=new personaje;
+Random rnd=new Random();
+int n=rnd.Next(5,11);
+
+personaje pj;
+fabricaDePersonajes generar=new fabricaDePersonajes();
+
+var Personajes=new List<personaje>();
+
+for (int i = 0; i < n; i++){
+    pj=generar.GeneraPersonajes();
+    Personajes.Add(pj);
+}
