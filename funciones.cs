aefd20d2bc13620afa3839b4d48427f50ebe3d33
“lucianobonilla27” <lucianobonilla27@gmail.com>
2023-06-15 17:38:22 -0300

    primer commit

---
+++ funciones.cs
@@ -0,0 +1,15 @@
+namespace funciones{
+    public class personajes{
+        private int velocidad; //1 a 10
+        private int destreza; // 1 a 5
+        private int fuerza; // 1 a 10
+        private int nivel;
+        private int armadura;
+        private int salud;
+        private string tipo;
+        private string nombre;
+        private string apellido;
+        private DateTime fechaNac;
+        private int edad;
+    }
+}
